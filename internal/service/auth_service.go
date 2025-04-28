d2a4fae1c10201e2e4d8c3f2b3cd366bdfe609c4
Alexx108 <alexx1088@gmail.com>
2025-04-28 22:50:52 +0500

    add migration

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -1,14 +1,17 @@
 package service
 
 import (
-	pb "authservice/proto/proto"
 	"context"
 	"errors"
 	"fmt"
+	"github.com/Alexx1088/authservice/internal/dto"
+	pb "github.com/Alexx1088/authservice/proto"
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// AuthServiceServer implements AuthServiceServer
 type AuthServiceServer struct {
 	pb.UnimplementedAuthServiceServer
 }
@@ -23,13 +26,33 @@ func (s *AuthServiceServer) Register(ctx context.Context, req *pb.RegisterReques
 	if req.GetEmail() == "" || req.GetPassword() == "" {
 		return nil, errors.New("email or password are required")
 	}
-	// 2. Hash the password
+
+	// 2. Convert RegisterRequest to DTO for internal processing
+	registerDTO := dto.RegisterRequestDTO{
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
+		Name:     req.GetName(),
+		Surname:  req.GetSurname(),
+	}
+
+	// 3. Validate DTO
+	validate := validator.New()
+	err := validate.Struct(registerDTO)
+	if err != nil {
+		return nil, fmt.Errorf("validation error: %w", err)
+	}
+
+	// 3. Hash the password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.GetPassword()), bcrypt.DefaultCost)
 	if err != nil {
 		return nil, fmt.Errorf("failed to hash password: %w", err)
 	}
-	// 3. Save the user to the database
+	// 4. Save the user to the database
 	userID := uuid.New().String()
+
+	// 5. Generate a token (here we mock it)
+
+	// 6. Map DTO back to Protobuf AuthResponse
 
 	response := &pb.AuthResponse{
 		Token: token,
